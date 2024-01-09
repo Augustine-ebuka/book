81836855721f09b2128f04ae30b26d0f956b971a
Obetta Augustine <hebrusgraphics@gmail.com>
2024-01-09 12:10:52 -0800

    adjusted readme

--- frontend/book-search/src/pages/home.tsx
+++ frontend/book-search/src/pages/home.tsx
@@ -71,7 +71,7 @@ function Home() {
           >
             <path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" />
           </svg>
-          <span className="font-semibold text-xl tracking-tight capitalize">Welcome back {userInfo?.data?.firstname}!</span>
+          <span className="font-semibold text-xl tracking-tight capitalize">Welcome back {userInfo?.firstname}!</span>
         </div>
         <div>
           <a href="#" className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-blue-600 hover:bg-white mt-4 lg:mt-0" onClick={logout}>
